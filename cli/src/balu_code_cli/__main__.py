85292d52bf280b6f020b5c54957a52cac437a33a
Xveyn <xveyn@baluhost.com>
2026-04-26 17:42:29 +0200

    feat(cli): wire session + config commands into __main__.py

--- cli/src/balu_code_cli/__main__.py
+++ cli/src/balu_code_cli/__main__.py
@@ -7,9 +7,11 @@ import typer
 from balu_code_cli import __version__
 from balu_code_cli.commands.auth import app as auth_app
 from balu_code_cli.commands.chat import app as chat_app
+from balu_code_cli.commands.config import app as config_app
 from balu_code_cli.commands.index import app as index_app
 from balu_code_cli.commands.init import app as init_app
 from balu_code_cli.commands.models import app as models_app
+from balu_code_cli.commands.session import app as session_app
 
 app = typer.Typer(
     name="balu-code",
@@ -22,6 +24,8 @@ app.add_typer(init_app, name="init")
 app.add_typer(models_app, name="models")
 app.add_typer(index_app, name="index")
 app.add_typer(chat_app, name="chat")
+app.add_typer(session_app, name="session")
+app.add_typer(config_app, name="config")
 
 
 def _version_callback(value: bool) -> None:
